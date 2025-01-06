0e846bb8bbbddd3b50b6aa44244ca249f0d2344e
Samuel Oliveira <samueloliveira900@gmail.com>
2025-01-06 18:48:08 -0300

    feat: adicionado Card com dolar rate, header e footer a view home

---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow text-center flex flex-col justify-center items-center m-10">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"  class="w-12 h-12">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+      </svg>
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900">Valor do Dólar</h5>
+        <p class="mb-3 text-lg font-medium text-center text-gray-700">
+            <span class="text-3xl">$ {{ value }}</span>
+        </p>
+    </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    value: 0,
+  }),
+  beforeCreate() {
+    const appId = import.meta.env.VITE_APP_ID;
+    const url = `https://openexchangerates.org/api/latest.json?app_id=${appId}`;
+
+    axios.get(url)
+    .then((response) => {
+      let dolar = response.data.rates.BRL
+      this.value = parseFloat(dolar).toFixed(2);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,24 @@
 <template>
-  <h2>Home</h2>
+  <Header></Header>  
+    <Card></Card>  
+
+  <Footer></Footer>
 </template>
-    
-<script setup>
+
+<script>
+import axios from "axios";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import Card from "@/components/Card.vue";
+
+export default {
+  data: () => ({
+    value: 0,
+  }),
+  components: {
+    Header,
+    Footer,
+    Card
+  }
+};
 </script>
